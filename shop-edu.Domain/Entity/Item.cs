d45869f483a44477048ae73c04cd8b2b806036c1
dolbolesya <zylynzy@gmail.com>
2023-08-26 20:46:15 +0300

    Initial commit

---
+++ shop-edu.Domain/Entity/Item.cs
@@ -0,0 +1,6 @@
+﻿namespace shop_edu.Domain.Entity;
+
+public class Item
+{
+    
+}
---
+++ shop-edu/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace shop_edu.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
